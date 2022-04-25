66f753a8909328eca557b11199d3c4442df407b3
Xavier Garcia Buils <xgbuils@gmail.com>
2022-04-25 12:36:35 +0200

    first commit

---
+++ src/index.mjs
@@ -0,0 +1,99 @@
+const isPromise = (obj) => typeof obj.then === "function";
+const isIterable = (obj) => obj && typeof obj[Symbol.iterator] === "function";
+
+const set = () => {
+  return true;
+};
+const bind = (target, prop) => {
+  if (typeof target[prop] === "function") {
+    return target[prop].bind(target);
+  }
+  return target[prop];
+};
+
+const promiseHandler = {
+  get: (resolver, prop) => {
+    const promise = resolver();
+    if (prop === "then") {
+      return functionProxy(bind(promise, prop));
+    }
+    const nextPromise = promise.then((target) =>
+      flatPromiseProxy(bind(noPromiseProxy(target), prop))
+    );
+    return promiseProxy(() => nextPromise);
+  },
+  apply: (resolver, thisArg, args) => {
+    return flatPromiseProxy(
+      resolver().then((f) => {
+        return flatPromiseProxy(f(...args));
+      })
+    );
+  },
+  set,
+};
+
+const promiseProxy = (resolver) => new Proxy(resolver, promiseHandler);
+
+const objectHandler = {
+  get: (target, prop) => {
+    return flatPromiseProxy(bind(target, prop));
+  },
+  set,
+};
+const objectProxy = (value) => new Proxy(value, objectHandler);
+
+const functionHandler = {
+  apply: (target, thisArg, args) => {
+    return flatPromiseProxy(target.apply(thisArg, args));
+  },
+  set,
+};
+const functionProxy = (fn) => {
+  return new Proxy(fn, functionHandler);
+};
+
+const hasPromiseItem = (iterable) => {
+  for (const item of iterable) {
+    if (isPromise(item)) {
+      return true;
+    }
+  }
+  return false;
+};
+
+const iterableProxy = (iterable) => {
+  if (hasPromiseItem(iterable)) {
+    const nextPromise = Promise.all(iterable);
+    return promiseProxy(() => nextPromise);
+  }
+  return objectProxy(iterable);
+};
+
+const noPromiseProxy = (value) => {
+  if (typeof value === "function") {
+    return functionProxy(value);
+  }
+  if (!value || typeof value !== "object") {
+    return value;
+  }
+  if (isIterable(value)) {
+    return iterableProxy(value);
+  }
+  return objectProxy(value);
+};
+
+export const flatPromiseProxy = (value) => {
+  if (typeof value === "function") {
+    return functionProxy(value);
+  }
+  if (!value || typeof value !== "object") {
+    return value;
+  }
+  if (isIterable(value)) {
+    return iterableProxy(value);
+  }
+  if (isPromise(value)) {
+    return promiseProxy(() => value);
+  }
+  return objectProxy(value);
+};
---
+++ src/test.mjs
@@ -0,0 +1,116 @@
+import assert from "assert";
+import { flatPromiseProxy } from "./index.mjs";
+
+describe("flatPromiseProxy", () => {
+  describe("object", () => {
+    it("target is an object", () => {
+      const proxy = flatPromiseProxy({ foo: "bar" });
+      assert.deepEqual(proxy, { foo: "bar" });
+      assert.deepEqual(proxy.foo, "bar");
+    });
+
+    it("target is a function that returns an object", () => {
+      const proxy = flatPromiseProxy(() => ({ foo: "bar" }));
+      assert.deepEqual(proxy(), { foo: "bar" });
+      assert.deepEqual(proxy().foo, "bar");
+    });
+
+    it("target is a promise that resolves to an object", async () => {
+      const proxy = flatPromiseProxy(Promise.resolve({ foo: "bar" }));
+      assert.deepEqual(await proxy, { foo: "bar" });
+      assert.deepEqual(await proxy.foo, "bar");
+    });
+  });
+
+  describe("function", () => {
+    it("target is a function", () => {
+      const proxy = flatPromiseProxy(() => "value");
+      assert.deepEqual(proxy(), "value");
+    });
+
+    it("target is a promise that resolves to a function", async () => {
+      const proxy = flatPromiseProxy(Promise.resolve(() => "value"));
+      assert.deepEqual(await proxy(), "value");
+    });
+
+    it("target is a promise that resolves to an object that resolves to a function", async () => {
+      const proxy = flatPromiseProxy(
+        Promise.resolve({ foo: () => Promise.resolve("bar") })
+      );
+      assert.deepEqual(await proxy.foo(), "bar");
+    });
+  });
+
+  describe("promise", () => {
+    it("target is a promise that resolves to a primitive", async () => {
+      const proxy = flatPromiseProxy(Promise.resolve(3));
+      assert.deepEqual(await proxy, 3);
+    });
+
+    it("target is an object with a prop that resolves to a promise that resolves to an object", async () => {
+      const proxy = flatPromiseProxy({
+        foo: Promise.resolve({
+          fizz: "buzz",
+        }),
+      });
+      assert.deepEqual(await proxy.foo.fizz, "buzz");
+    });
+
+    it("target is an object with a method that resolves to a promise that resolves to an object", async () => {
+      const proxy = flatPromiseProxy({
+        foo: () =>
+          Promise.resolve({
+            fizz: "buzz",
+          }),
+      });
+      assert.deepEqual(await proxy.foo().fizz, "buzz");
+    });
+
+    it("target is promise that resolves to an object with a method that resolves to a promise that resolves to an object", async () => {
+      const proxy = flatPromiseProxy(
+        Promise.resolve({
+          foo: () =>
+            Promise.resolve({
+              fizz: "buzz",
+            }),
+        })
+      );
+      assert.deepEqual(await proxy.foo(), {
+        fizz: "buzz",
+      });
+      assert.deepEqual(await proxy.foo().fizz, "buzz");
+    });
+  });
+
+  describe("arrays", () => {
+    it("target is an array without promise items", () => {
+      const proxy = flatPromiseProxy([1, 2, 3]);
+      const transformedProxy = proxy.map((x) => 2 * x);
+      assert.deepEqual(transformedProxy, [2, 4, 6]);
+    });
+
+    it("mapping an array with promise items", async () => {
+      const proxy = flatPromiseProxy([1, Promise.resolve(2), 3]);
+      const transformedProxy = await proxy.map((x) => 2 * x);
+      assert.deepEqual(transformedProxy, [2, 4, 6]);
+    });
+
+    it("filtering an array with promise items", async () => {
+      const proxy = flatPromiseProxy([
+        1,
+        Promise.resolve(2),
+        Promise.resolve(3),
+      ]);
+      const transformedProxy = await proxy.filter((x) => x % 2 !== 0);
+      assert.deepEqual(transformedProxy, [1, 3]);
+    });
+
+    it("target is a promise that resolves to an array", async () => {
+      const proxy = flatPromiseProxy(
+        Promise.resolve([1, Promise.resolve(2), Promise.resolve(3)])
+      );
+      const transformedProxy = await proxy.filter((x) => x % 2 !== 0);
+      assert.deepEqual(transformedProxy, [1, 3]);
+    });
+  });
+});
